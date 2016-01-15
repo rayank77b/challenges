588850660def666b1bc8d7ab00a7944f45c59dc3
Andrej Frank <Andrej.Frank@stz-softwaretechnik.de>
2016-01-15 15:19:37 +0100

    test fast read

---
+++ katas/TestFastRead.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+public class TestFastRead {
+
+	public static String fastRead() throws IOException {
+		String line = "";
+        int c;
+		while ((c = System.in.read()) >= 0) {
+			switch (c) {
+            	case '\n':
+            		break;
+            	default:
+                    line += (char) c;
+                    break;
+			}
+		}
+		return line;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("start");
+		String ein;
+		try {
+			ein = fastRead();
+			System.out.println("readed: "+ein);
+		} catch (IOException e) {
+			System.out.println("errors on read");
+		}
+		
+
+	}
+
+}
